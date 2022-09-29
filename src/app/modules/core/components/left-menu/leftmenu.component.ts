c13cb86e9e71535e68dfd121538f1aa8b4adca25
Prathmesh Deo <prathmeshdeo13@gmail.com>
2022-09-29 12:47:34 +0530

    1.Changed the submenu names from Evaulation criteria master.

--- src/app/modules/core/components/left-menu/leftmenu.component.ts
+++ src/app/modules/core/components/left-menu/leftmenu.component.ts
@@ -46,88 +46,6 @@ export class LeftmenuComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // NumberMenuList:any =[
-  //      {
-  //     name:"Department",
-  //     isHeading:false,
-  //     iconClass:"icon-rocket",
-  //     dynamicMenuOpen:false,
-  //     routerLink: ['/performance/performance/department-table']
-  //       },
-  //       {
-  //         name:"Division",
-  //         isHeading:false,
-  //         iconClass:"icon-rocket",
-  //         dynamicMenuOpen:false,
-  //         routerLink: ['/performance/performance/division-table']
-  //       },
-  //       {
-  //         name:"Grade",
-  //         isHeading:false,
-  //         iconClass:"icon-rocket",
-  //         dynamicMenuOpen:false,
-  //         routerLink: ['/performance/performance/grades-table']
-  //       },
-  //       {
-  //         name:"Role",
-  //         isCollapsed: true,
-  //         isHeading:false,
-  //         iconClass:"icon-rocket",
-  //         dynamicMenuOpen:false,
-  //         routerLink: ['/performance/performance/role-table'],
-  //       },
-  //       {
-  //         name:"Designation",
-  //         isCollapsed: true,
-  //         isHeading:false,
-  //         iconClass:"icon-rocket",
-  //         dynamicMenuOpen:false,
-  //         routerLink: ['/performance/performance/designation-table'],
-  //       },
-  //       {
-  //         name:"Performance",
-  //         isCollapsed: false,
-  //         isHeading:false,
-  //         iconClass:"icon-rocket",
-  //         dynamicMenuOpen:false,
-
-  //         submenu:[
-  //           {
-  //             name:"Key_Performance_Areas",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Key_Performance_Areas_EmployeeGrades",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Key_Performance_Areas_EmployeeGrades",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Goals_Key_Performance_Areas_Roles",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Performance_Review_Type",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Performance_Review_Phases",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Performance_Review_Grades",
-  //             routerLink:[''],
-  //           },
-  //           {
-  //             name:"Performance_Review_Cycle_Schedule",
-  //             routerLink:[''],
-  //           },
-  //         ]
-  //       }
-
-  //     ]
   NumberMenuList: any = [
     {
       name: "Masters",
@@ -135,7 +53,6 @@ export class LeftmenuComponent implements OnInit {
       isCollapsed: true,
       isHeading: false,
       iconClass: "icon-rocket",
-      // dynamicMenuOpen: false,
       submenu: [
         {
           name: "Department",
@@ -160,7 +77,7 @@ export class LeftmenuComponent implements OnInit {
       ],
     },
     {
-      name: "Performance",
+      name: "Evaluation Criteria",
       controlId: "collapseBasicProduct",
       isCollapsed: true,
       isHeading: false,
@@ -169,31 +86,31 @@ export class LeftmenuComponent implements OnInit {
       translationKey: "product_main_menu_title",
       submenu: [
         {
-          name: "Key_Performance_Areas",
+          name: "Key Performance Areas",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Key_Performance_Areas_EmployeeGrades",
+          name: "Key Performance Areas EmployeeGrades",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Goals_Key_Performance_Areas_Roles",
+          name: "Goals Key Performance Areas Roles",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Performance_Review_Type",
+          name: "Performance Review Type",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Performance_Review_Phases",
+          name: "Performance Review Phases",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Performance_Review_Grades",
+          name: "Performance Review Grades",
           routerLink: ["/crm/crm/product-templates"]
         },
         {
-          name: "Performance_Review_Cycle_Schedule",
+          name: "Performance Review Cycle Schedule",
           routerLink: ["/crm/crm/product-templates"]
         },
       ],
@@ -203,7 +120,7 @@ export class LeftmenuComponent implements OnInit {
       isHeading: true,
       iconClass: "icon-rocket",
       isGheading: false,
-      routerLink: ["/crm/crm/product-configuration"],
+      routerLink: ["performance/performance/employee-table"],
       dynamicMenuOpen: false,
     },
   ];
