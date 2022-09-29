5627c0803e463c741fb7f5fc6e1a71743861ceb1
Prathmesh Deo <prathmeshdeo13@gmail.com>
2022-09-29 12:54:36 +0530

    1.Added new data table changes for department master

--- src/app/modules/performance/department/department.component.ts
+++ src/app/modules/performance/department/department.component.ts
@@ -1,16 +1,18 @@
+import { DepartmentService } from "./../services/department.service";
 import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { PerformanceService } from "../services/performance.service";
 import { forkJoin } from "rxjs";
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { HttpParams } from "@angular/common/http";
+import { Department } from "../model/department.model";
 
 @Component({
   selector: "app-department",
   templateUrl: "./department.component.html",
   styleUrls: ["./department.component.scss"],
-  providers: [PerformanceService],
+  providers: [PerformanceService ,DepartmentService],
 })
 export class DepartmentComponent implements OnInit {
   actionbtn1: string;
@@ -20,19 +22,23 @@ export class DepartmentComponent implements OnInit {
   permission: Array<boolean> = [true, true, true];
   params: HttpParams = new HttpParams();
   departmentForm: FormGroup;
+  actionBtn: "save";
+  res: any;
 
   constructor(
     private router: Router,
     private performanceService: PerformanceService,
     private modalService: BsModalService,
-    private formbuilder: FormBuilder
-  )
-   { this.departmentForm = this.initForm();
+    private formbuilder: FormBuilder,
+    private departmentService: DepartmentService
+  ) {
+    this.departmentForm = this.initForm();
   }
 
   ngOnInit(): void {
-    this.params = this.params.append('offset', 0);
-    this.params = this.params.append('limit', 5);
+
+    this.params = this.params.append("offset", 0);
+    this.params = this.params.append("limit", 5);
     forkJoin({
       tableHeader: this.performanceService.getHeaderColumes(),
       tableData: this.performanceService.getContentColumes(this.params),
@@ -41,45 +47,58 @@ export class DepartmentComponent implements OnInit {
         console.log(response);
 
         this.columnsMetadata = response.tableHeader;
-
         this.dataDataTable = response.tableData;
       },
 
       (error) => {}
     );
+
+    if(this.res =! undefined){
+      
+    }
   }
 
-  changePageSortSearch(data:HttpParams){
-this.performanceService.getContentColumes(data).subscribe((sucess:any)=>{
-  this.dataDataTable = sucess;
-  
-})
-  }
- 
-  initForm(): FormGroup {
-    return this.formbuilder.group({
-
-      dept_code: ['', Validators.required],
-      dept_name: ['', Validators.required],
-      dept_description: ['', Validators.required],
-      org_code:['dept', Validators.required]
+  changePageSortSearch(data: HttpParams) {
+    this.performanceService.getContentColumes(data).subscribe((sucess: any) => {
+      this.dataDataTable = sucess;
     });
   }
 
+  initForm(): FormGroup {
+    return this.formbuilder.group({
+      dept_code: ["", Validators.required],
+      dept_name: ["", Validators.required],
+      dept_description: ["", Validators.required],
+      org_code: ["AVISYS", Validators.required],
+      is_deleted: [false],
+      created_by: ["1"],
+      updated_by: ["1"],
+    });
+  }
 
   editConsumerAttribute(template2) {
-    this.modalRef = this.modalService.show(template2, Object.assign({}, { class: 'gray modal-lg ' }));
+    this.modalRef = this.modalService.show(
+      template2,
+      Object.assign({}, { class: "gray modal-lg " })
+    );
   }
 
   submit() {
-    console.log(this.departmentForm.value)
+    console.log(this.departmentForm.value);
   }
 
-  buttonEvent1(data: any , template2) {
+
+  buttonEvent1(data: any, template2) {
     if (data.event == "add") {
-      this.editConsumerAttribute(template2)
+      this.departmentForm.reset();
+      this.editConsumerAttribute(template2);
     } else if (data.event == "edit") {
-     
+      console.log(data);
+      this.departmentService.getById(data.data.dept_code).subscribe((res) => {
+        this.editConsumerAttribute(template2);
+        this.departmentForm.patchValue(res);
+        this.res = res;
+      });
     } else if (data.event == "delete") {
     }
   }
