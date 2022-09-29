252db388320d8de9f6af5ceb2205806355408dcd
Prathmesh Deo <prathmeshdeo13@gmail.com>
2022-09-29 13:23:12 +0530

    Did changes for Data table for Grade master

--- src/app/modules/performance/grade/grade.component.ts
+++ src/app/modules/performance/grade/grade.component.ts
@@ -1,14 +1,16 @@
+import { GradeService } from './../services/grade.service';
 import { PerformanceService } from "./../services/performance.service";
 import { Component, OnInit } from "@angular/core";
 import { forkJoin } from "rxjs";
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { HttpParams } from "@angular/common/http";
 
 @Component({
   selector: "app-grade",
   templateUrl: "./grade.component.html",
   styleUrls: ["./grade.component.scss"],
-  providers: [PerformanceService],
+  providers: [PerformanceService , GradeService],
 })
 export class GradeComponent implements OnInit {
   columnsMetadata: any;
@@ -16,18 +18,22 @@ export class GradeComponent implements OnInit {
   dataDataTable: any;
   modalRef: BsModalRef;
   gradeForm: FormGroup;
+  params: HttpParams = new HttpParams();
 
   constructor(
     private performanceService: PerformanceService,
     private formBuilder: FormBuilder,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private gradeService : GradeService
   ) {this.gradeForm = this.initForm();}
 
   ngOnInit(): void {
+    this.params = this.params.append('offset', 0);
+    this.params = this.params.append('limit', 5);
     forkJoin({
       tableHeader: this.performanceService.getGradeHeaderColumes(),
 
-      tableData: this.performanceService.getGradeContentColumes(),
+      tableData: this.gradeService.getDivisionContentColumes2(this.params),
     }).subscribe(
       (response) => {
         console.log(response);
@@ -41,6 +47,13 @@ export class GradeComponent implements OnInit {
     );
   }
 
+  changePageSortSearch(data: HttpParams) {
+    this.gradeService.getDivisionContentColumes2(data).subscribe((sucess: any) => {
+      this.dataDataTable = sucess;
+    });
+  }
+
+
   editConsumerAttribute(template2) {
     this.modalRef = this.modalService.show(
       template2,
@@ -50,9 +63,12 @@ export class GradeComponent implements OnInit {
 
   initForm(): FormGroup {
     return this.formBuilder.group({
-      gradeId: ["", Validators.required],
-      gradeName: ["", Validators.required],
-      gradeDescription: ["", Validators.required],
+      grade_code: ["", Validators.required],
+      grade_name: ["", Validators.required],
+      org_code: ["AVISYS", Validators.required],
+      is_deleted: [false],
+      created_by: ["1"],
+      updated_by: ["1"]
     });
   }
 
@@ -60,7 +76,10 @@ export class GradeComponent implements OnInit {
     if (data.event == "add") {
     this.editConsumerAttribute(template2)
     } else if (data.event == "edit") {
-      // this.router.navigate(['crm/crm/product-family-form'], { queryParams: { data: JSON.stringify(data.data.id) } });
+      this.gradeService.getById(data.data.grade_code).subscribe((res) => {
+        this.editConsumerAttribute(template2);
+        this.gradeForm.patchValue(res);
+      });
     } else if (data.event == "delete") {
     }
   }
