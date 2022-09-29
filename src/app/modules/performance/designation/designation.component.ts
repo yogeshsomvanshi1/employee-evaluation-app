ab4c8a62475c0c18cc7c3f9a1989b504a922b608
Prathmesh Deo <prathmeshdeo13@gmail.com>
2022-09-29 13:20:41 +0530

    Did changes for Data Table for Designation master

--- src/app/modules/performance/designation/designation.component.ts
+++ src/app/modules/performance/designation/designation.component.ts
@@ -1,14 +1,16 @@
+import { DesignationService } from './../services/designation.service';
 import { PerformanceService } from "./../services/performance.service";
 import { Component, OnInit } from "@angular/core";
 import { forkJoin } from "rxjs";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { HttpParams } from "@angular/common/http";
 
 @Component({
   selector: "app-designation",
   templateUrl: "./designation.component.html",
   styleUrls: ["./designation.component.scss"],
-  providers: [PerformanceService],
+  providers: [PerformanceService , DesignationService],
 })
 export class DesignationComponent implements OnInit {
   columnsMetadata: any;
@@ -16,18 +18,22 @@ export class DesignationComponent implements OnInit {
   dataDataTable: any;
   modalRef: BsModalRef;
   designationForm: FormGroup;
+  params:  HttpParams = new HttpParams();
 
   constructor(
     private performanceService: PerformanceService,
     private formBuilder: FormBuilder,
     private modalService: BsModalService,
+    private designationService : DesignationService
   ) {this.designationForm = this.initForm();}
 
   ngOnInit(): void {
+    this.params = this.params.append('offset', 0);
+    this.params = this.params.append('limit', 5);
     forkJoin({
       tableHeader: this.performanceService.getDesignationHeaderColumes(),
 
-      tableData: this.performanceService.getDesignationContentColumes(),
+      tableData: this.designationService.getDesignationContentColumes4(this.params),
     }).subscribe(
       (response) => {
         console.log(response);
@@ -41,6 +47,13 @@ export class DesignationComponent implements OnInit {
     );
   }
 
+  
+  changePageSortSearch(data: HttpParams) {
+    this.designationService.getDesignationContentColumes4(data).subscribe((sucess: any) => {
+        this.dataDataTable = sucess;
+      });
+  }
+
 
   editConsumerAttribute(template2) {
     this.modalRef = this.modalService.show(template2, Object.assign({}, { class: "gray modal-lg " })
@@ -49,9 +62,13 @@ export class DesignationComponent implements OnInit {
 
   initForm(): FormGroup {
     return this.formBuilder.group({
-      designationId: ["", Validators.required],
-      designationName: ["", Validators.required],
-      designationDescription: ["", Validators.required],
+      des_code: ['', Validators.required],
+      des_name: ['', Validators.required],
+      des_description: ['', Validators.required],
+      org_code:['AVISYS', Validators.required],
+      is_deleted:[false ],
+      created_by:['1'],
+      updated_by:['1']
     });
   }
 
@@ -60,7 +77,11 @@ export class DesignationComponent implements OnInit {
     if (data.event == "add") {
       this.editConsumerAttribute(template2)
     } else if (data.event == "edit") {
-      // this.router.navigate(['crm/crm/product-family-form'], { queryParams: { data: JSON.stringify(data.data.id) } });
+      this.designationService.getById(data.data.des_code).subscribe((res) => {
+      this.editConsumerAttribute(template2);
+      this.designationForm.patchValue(res);
+      });
+    
     } else if (data.event == "delete") {
     }
   }
