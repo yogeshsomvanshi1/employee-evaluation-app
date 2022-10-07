2bcd5af1c7bb40ea9aeba9dd668598fa3541fd4d
Hrishikesh-1998 <rane.hrishi09.1998@gmail.com>
2022-10-07 17:56:24 +0530

    A) Added Validations and removed unused code from component B) Added Reset logic c) Added Alert message D) Made function name correction

--- src/app/modules/performance/division/division.component.ts
+++ src/app/modules/performance/division/division.component.ts
@@ -1,124 +1,137 @@
+import { Division } from './../model/division.model';
 import { DivisionService } from './../services/division.service';
 import { PerformanceService } from "./../services/performance.service";
-import { Router } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
 import { forkJoin } from "rxjs";
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { HttpParams } from "@angular/common/http";
+import { AlertOptions } from '../../shared/model/alert.model';
+import { ValidatorServiceService } from '../../shared/component/validator-service/validator-service.service';
+import { AlertService } from '../../shared/services/alert.service';
+import { TableHeaderMetaData } from '../../shared/model/table-header-list.model';
 
 @Component({
-  selector: "app-division",
-  templateUrl: "./division.component.html",
-  styleUrls: ["./division.component.scss"],
-  providers: [PerformanceService , DivisionService],
+	selector: "app-division",
+	templateUrl: "./division.component.html",
+	styleUrls: ["./division.component.scss"],
 })
 export class DivisionComponent implements OnInit {
-  columnsMetadata: any;
-  permission: Array<boolean> = [true, true, true];
-  dataDataTable: any;
-  modalRef: BsModalRef;
-  divisionForm: FormGroup;
-  params: HttpParams = new HttpParams();
-  submitBtn: string = "submit";
-  res: any;
-  response: any;
+	@ViewChild('divisionTemplate') divisionTemplate: TemplateRef<BsModalRef>;
+	
+	alertOptions: AlertOptions = { autoClose: true, keepAfterRouteChange: true };
+	actionBtn: string = "Submit";
+	columnsMetadata: TableHeaderMetaData;
+	dataDataTable: { results: Array<Division>, count: number } = { results: [], count: 0 };
+	divisionForm: FormGroup;
+	defaultIntialValue: Division;
+	intialValue: Division;
+	modalRef: BsModalRef;
+	permission: Array<boolean> = [true, true, true];
+	params: HttpParams = new HttpParams();
 
-  constructor(
-    private router: Router,
-    private performanceService: PerformanceService,
-    private formbuilder: FormBuilder,
-    private modalService: BsModalService,
-    private divisionService : DivisionService
-  ) {
-    this.divisionForm = this.initForm();
-  }
+	constructor(
+		private alertService: AlertService,
+		private divisionService: DivisionService,
+		private formbuilder: FormBuilder,
+		private modalService: BsModalService,
+		private pattern: ValidatorServiceService,
+		private performanceService: PerformanceService,
+	) {
+		this.divisionForm = this.initForm();
+	}
 
-  ngOnInit(): void {
-    this.params = this.params.append('offset', 0);
-    this.params = this.params.append('limit', 5);
-    forkJoin({
-      tableHeader: this.performanceService.getDivisionHeaderColumes(),
+	ngOnInit(): void {
+		this.defaultIntialValue = this.divisionForm.value;
+		this.params = this.params.append('offset', 0);
+		this.params = this.params.append('limit', 5);
+		forkJoin({
+			tableHeader: this.performanceService.getDivisionHeaderColumn(),
+			tableData: this.divisionService.getDivisionContent(this.params),
+		}).subscribe(
+			(response:any) => {
+				this.columnsMetadata = response.tableHeader;
+				this.dataDataTable = response.tableData;
+			},
+			(error) => { }
+		);
+	}
 
-      tableData: this.divisionService.getDivisionContentColumes1(this.params),
-    }).subscribe(
-      (response) => {
-        console.log(response);
+	openTemplate() {
+		this.modalRef = this.modalService.show(this.divisionTemplate, Object.assign({}, { class: "gray modal-lg " })
+		);
+	}
 
-        this.columnsMetadata = response.tableHeader;
+	changePageSortSearch(data: HttpParams) {
+		this.divisionService.getDivisionContent(data).subscribe((sucess: { results: Array<Division>, count: number }) => {
+			this.dataDataTable = sucess;
+		});
+	}
 
-        this.dataDataTable = response.tableData;
-      },
+	initForm(): FormGroup {
+		return this.formbuilder.group({
+			div_code: ["",  [Validators.required, Validators.maxLength(10)]],
+			div_name: ["",  [Validators.required, Validators.maxLength(50)]],
+			div_description: ["", [Validators.required, Validators.maxLength(500),Validators.pattern(this.pattern.descriptionValidation())]],
+			org_code: ["AVISYS"],
+			is_deleted: [false],
+			created_by: ["1"],
+			updated_by: ["1"],
+		});
+	}
 
-      (error) => {}
-    );
-  }
+	submit() {
 
-  editConsumerAttribute(template2) {
-    this.modalRef = this.modalService.show(template2, Object.assign({}, { class: "gray modal-lg " })
-    );
-  }
+		if (this.actionBtn !== "Submit") {
+			this.divisionService.update(this.divisionForm.getRawValue(), this.divisionFormControl.div_code.value).subscribe((response:Division) => {
+				this.changePageSortSearch(this.params);
+				this.alertService.success("Record Updated Successfully", this.alertOptions.autoClose);
+				this.modalRef.hide();
+			});
+		} 
+		else {
+			this.divisionService.create(this.divisionForm.value).subscribe((sucess:Division) => {
+				this.changePageSortSearch(this.params);
+				this.alertService.success("Record Added Successfully", this.alertOptions.autoClose);
+				this.modalRef.hide();
+			}, (error)=>{
+				if(error.error.div_code){
+					this.alertService.info("Record already exists", this.alertOptions.autoClose = false );
+				}
+			});
+		}
+	}
 
-  changePageSortSearch(data: HttpParams) {
-    this.divisionService.getDivisionContentColumes1(data).subscribe((sucess: any) => {
-      this.dataDataTable = sucess;
-    });
-  }
+	buttonEvent1(data: any) {
+		if (data.event == "add") {
+			this.openTemplate()
+			this.actionBtn = "Submit";
+			this.divisionFormControl.div_code.enable();
+			this.resetForm();
+		}
+		else if (data.event == "edit") {
+			this.divisionService.getById(data.data.div_code).subscribe((res) => {
+				this.openTemplate();
+				this.actionBtn = "Update";
+				this.divisionFormControl.div_code.disable();
+				this.divisionForm.patchValue(res);
+				this.intialValue = res;
+			});
+		}
+		else if (data.event == "delete") {
+			this.divisionService.softDelete(data.data.div_code).subscribe((sucess:Division) => {
+				this.alertService.success("Record Deleted Successfully", this.alertOptions.autoClose);
+				this.changePageSortSearch(this.params);
+			})
+		}
+	}
 
-  initForm(): FormGroup {
-    return this.formbuilder.group({
-      div_code: ["", Validators.required],
-      div_name: ["", Validators.required],
-      div_description: ["", Validators.required],
-      // org_code: ["AVISYS", Validators.required],
-      // is_deleted: [false],
-      // created_by: ["1"],
-      // updated_by: ["1"],
-    });
-  }
+	get divisionFormControl(): { [key: string]: AbstractControl } {
+		return this.divisionForm.controls;
+	}
 
-  submit() {
-    this.divisionService.create(this.divisionForm.value).subscribe((res) => {
-      this.res;
-    });
-
-  if (this.submitBtn != "submit") {
-    this.update(this.divisionForm.controls["div_code"].value);
-    this.changePageSortSearch(this.params);
-  } else {
-    this.divisionService.create(this.divisionForm.value).subscribe((sucess) => {
-      });
-  }
-}
-
-update(id: number) {
-  this.divisionService.update(this.divisionForm.value, id).subscribe((response) => {
-    this.response;
-  });
-  }
-
-
-
-  buttonEvent1(data: any, template2) {
-    if (data.event == "add") {
-      this.editConsumerAttribute(template2)
-      this.submitBtn = "Submit";
-      this.divisionForm.reset();
-    } else if (data.event == "edit") {
-      this.divisionService.getById(data.data.div_code).subscribe((res) => {
-        this.editConsumerAttribute(template2);
-        this.submitBtn = "Update";
-        this.divisionForm.patchValue(res);
-        this.res = res;
-      });
-    } else if (data.event == "delete") {
-      this.divisionService.softDelete(data.data.div_code).subscribe((sucess1)=>{
-        this.changePageSortSearch(this.params);
-       })
-    }
-  }
-
-  
- 
+	resetForm() {
+		this.divisionForm.reset(this.actionBtn === 'Submit' ? this.defaultIntialValue : this.intialValue);
+	}
 
 }
