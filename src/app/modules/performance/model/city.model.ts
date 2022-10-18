d9dcc9a56928ce3098fbe8780037ba285a11a08d
Hrishikesh-1998 <rane.hrishi09.1998@gmail.com>
2022-10-18 15:00:38 +0530

    1) Model files of component 2) Model of header's

---
+++ src/app/modules/performance/model/city.model.ts
@@ -0,0 +1,11 @@
+
+
+export interface City{
+    "country_code": string,
+    "state_code":string,
+    "city_code":string,
+    "city_name":string,
+    "is_deleted": boolean,
+    "created_by": string,
+    "updated_by": string
+}
---
+++ src/app/modules/performance/model/employee-type.model.ts
@@ -0,0 +1,9 @@
+
+export interface EmployeeType {
+    "emp_type_code": string,
+    "emp_type_name": string,
+    "is_deleted": boolean,
+    "created_by": string,
+    "updated_by": string,
+    "org_code": string
+}
